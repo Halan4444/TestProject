61b4d7841f7270d19cb23c850d8e17846a16aa45
Trần Hạ Lan <halan4444b@gmail.com>
2022-03-11 14:18:28 +0700

    Long2

---
+++ Student.js
@@ -0,0 +1,20 @@
+class Student {
+    constructor(id,name,age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    getId () {
+        return this.id;
+    } setID (id) {
+        this.id = id;
+    } getName() {
+        return this.name;
+    } setName(name) {
+        this.name = name
+    } getAge () {
+        return this.age;
+    } setAge (age) {
+        this.age = age;
+    }
+}
